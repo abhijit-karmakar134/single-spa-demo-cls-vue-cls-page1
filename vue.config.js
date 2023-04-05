c15d656a522d05c407c9e2ee9207a3986385bc1d
mfAppRepo <ashutosh.sharma@highradius.com>
2023-04-05 20:18:13 +0530

    Commit Changes

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  // various other settings such as publicPath, etc
+  // ...
+  configureWebpack: {
+    output: {
+      libraryTarget: 'system',
+    },
+  },
+}
